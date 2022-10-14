b2deef41e11c3de17c0a695658f11210da449399
lloydkioko <kiokolloyd@gmail.com>
2022-10-14 04:37:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+/* Prints string */
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - contains main program code
+ * Return: print string; 0 otherwise
+ */
+
+
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
