80a3ae4c2e4bba4e1321c391980657e158751bd1
lloydkioko <kiokolloyd@gmail.com>
2022-10-18 04:13:40 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: prints hexadecimal numbers in lowercase; 0 otherwise
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
