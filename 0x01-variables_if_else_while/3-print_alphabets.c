c8b95be8e206a18a91284179347cab09c725ad09
lloydkioko <kiokolloyd@gmail.com>
2022-10-18 03:36:38 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: prints alphabet in lowercase; 0 otherwise
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
