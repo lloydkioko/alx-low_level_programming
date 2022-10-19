e0a28a21bd223ff913e7d5797f486abe61dda52e
lloydkioko <kiokolloyd@gmail.com>
2022-10-19 13:13:20 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,43 @@
 #include "main.h"
+
 /**
- *Description:print_times_table- Print timetables for n
- *@n:variable for multiplication
- */
+  * print_times_table - prints n times table, from zero
+  * @n: number to print times table of
+  * Return: null if n > 15 or n < 0
+  */
 void print_times_table(int n)
 {
-int i, j, res;
-if (!(n > 15 || n < 0))
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-res = (i * j);
-if (j != 0)
-{
-putchar(',');
-putchar(' ');
-}
-if (res < 10 && j != 0)
-{
-putchar(' ');
-putchar(' ');
-putchar((res % 10) + '0');
-}
-else if (res >= 10 && res < 100)
-{
-putchar(' ');
-putchar((res / 10) + '0');
-putchar((res % 10) + '0');
-}
-else if (res >= 100 && j != 0)
-{
-putchar((res / 100) + '0');
-putchar((res / 10) % 10 + '0');
-putchar((res % 10) + '0');
-}
-else
-{
-putchar((res % 10) + '0');
-}
-}
-putchar('\n');
-}
-}
+	int i, j, k, l, m;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+
+			if (k > n)
+			{
+				l = k % 10;
+				m = (k - l) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(m + '0');
+				_putchar(l + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(k + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
 }
