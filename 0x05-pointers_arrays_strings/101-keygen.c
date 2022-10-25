d17496265038488e76abe8ddc4529e0744b486f0
lloydkioko <kiokolloyd@gmail.com>
2022-10-25 14:55:15 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,51 @@
 #include <time.h>
 
 /**
- * main - generates random passwords for 101-crackme
+ * main - Generates random valid pass
+ *
  * Return: Always 0
  */
 int main(void)
 {
-int sum;
-char c;
+	char pass[84];
+	int ind = 0, sum = 0, diff_half1, diff_half2;
 
-srand(time(NULL));
-while (sum <= 2645)
-{
-c = rand() % 128;
-sum += c;
-putchar(c);
-}
-putchar(2772 - sum);
+	srand(time(0));
 
-return (0);
+	while (sum < 2772)
+	{
+		pass[ind] = 33 + rand() % 94;
+		sum += pass[ind++];
+	}
+
+	pass[ind] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (ind = 0; pass[ind]; ind++)
+		{
+			if (pass[ind] >= (33 + diff_half1))
+			{
+				pass[ind] -= diff_half1;
+				break;
+			}
+		}
+		for (ind = 0; pass[ind]; ind++)
+		{
+			if (pass[ind] >= (33 + diff_half2))
+			{
+				pass[ind] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", pass);
+
+	return (0);
 }
