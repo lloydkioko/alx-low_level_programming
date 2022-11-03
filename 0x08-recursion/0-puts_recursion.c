35585680da09df30d3ad3081be4039276fdd39c4
lloydkioko <kiokolloyd@gmail.com>
2022-11-03 12:57:33 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  function that prints a string
+ *
+ * @s: string to receive
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s);
+{
+if (*s == '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
