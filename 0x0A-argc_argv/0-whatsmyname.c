9887131a442bb7718f19c7b0c22704182a1bb1c4
lloydkioko <kiokolloyd@gmail.com>
2022-11-07 13:19:17 -0800

    1-args.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints name of program followed by a new line
+ * @argc: number of arguments
+ * @argv: array containing arguments passed to the program
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
