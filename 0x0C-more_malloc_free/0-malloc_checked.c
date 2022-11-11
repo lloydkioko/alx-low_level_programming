aaaf0264145a999e6c7483d7ce23fccfeb3658aa
lloydkioko <kiokolloyd@gmail.com>
2022-11-11 13:13:50 -0800

    malloc addition

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: number of bytes to be allocated.
+ *
+ * Return: pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem_alloc = malloc(b);
+
+	if (mem_alloc == NULL)
+		exit(98);
+
+	return (mem_alloc);
+}
