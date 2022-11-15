1d9ff0a7a47653c668116efef243611d0ca28cdb
lloydkioko <kiokolloyd@gmail.com>
2022-11-15 15:15:08 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints name of file it's compiled from
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
