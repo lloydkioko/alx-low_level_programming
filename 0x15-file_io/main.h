61ca196f6ced77b056d8978a78451ea1c17efe70
lloydkioko <kiokolloyd@gmail.com>
2022-12-08 03:15:35 -0800

    File/io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,19 +14,3 @@ int create_file(const char *filename, char *text_content)
 int append_text_to_file(const char *filename, char *text_content)
 
 #endif
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-ssize_t read_textfile(const char *filename, size_t letters)
-int create_file(const char *filename, char *text_content)
-int append_text_to_file(const char *filename, char *text_content)
-
-#endif
